6b8291cf0b3aa33a43cd6ed2cf3a01723c2064a8
ngolongquan <longquan552@gmail.com>
2022-04-24 18:42:23 +0700

    update by comment

--- src/server.ts
+++ src/server.ts
@@ -32,7 +32,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   
   //! END @TODO1
   app.get("/filteredimage",
-      async ( req, res ) => {
+      async ( req:express.Request, res:express.Response ) => {
         let image_url = req.query.image_url;
         if (!image_url) {
           res.status(422).send("URL is empty");
